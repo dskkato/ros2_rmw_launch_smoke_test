4cf978bd98b821dd64f5ea9b835aed9e7a7bbb65
dskkato <kato.daisuke429@gmail.com>
2026-09-06 20:17:37 +0900

    Remove ineffective launch timing workaround

--- rmw_launch_smoke_test_cpp/test/test_pub_sub_launch_windows.py
+++ rmw_launch_smoke_test_cpp/test/test_pub_sub_launch_windows.py
@@ -78,8 +78,7 @@ def generate_test_description():
 
     actions.extend([
         TimerAction(period=1.0, actions=[subscriber]),
-        # Allow Fast DDS discovery to complete before publishing.
-        TimerAction(period=5.0, actions=[publisher]),
+        TimerAction(period=3.0, actions=[publisher]),
     ])
     actions.append(RegisterEventHandler(OnProcessExit(
         target_action=subscriber,
