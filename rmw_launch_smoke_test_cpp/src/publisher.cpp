6dff6a58ecc8553a65d69d0c5cfc55461b6c97db
dskkato <kato.daisuke429@gmail.com>
2026-09-06 19:54:10 +0900

    Use ROS clang format style

--- rmw_launch_smoke_test_cpp/src/publisher.cpp
+++ rmw_launch_smoke_test_cpp/src/publisher.cpp
@@ -21,15 +21,18 @@
 
 using namespace std::chrono_literals;
 
-class SmokePublisher : public rclcpp::Node {
+class SmokePublisher : public rclcpp::Node
+{
 public:
-  SmokePublisher() : Node("smoke_publisher"), count_(0) {
+  SmokePublisher() : Node("smoke_publisher"), count_(0)
+  {
     publisher_ = create_publisher<std_msgs::msg::String>("smoke_chatter", 10);
     publish_timer_ = create_wall_timer(200ms, [this]() { publish(); });
   }
 
 private:
-  void publish() {
+  void publish()
+  {
     std_msgs::msg::String message;
     message.data = "hello-" + std::to_string(count_);
     publisher_->publish(message);
@@ -48,7 +51,8 @@ private:
   int count_;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
   auto node = std::make_shared<SmokePublisher>();
   rclcpp::spin(node);
